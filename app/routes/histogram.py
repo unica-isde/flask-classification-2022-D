15d5c2fee82957a352fec60872ea9651e12f72a7
LoaCos83 <67106370+LoaCos83@users.noreply.github.com>
2022-11-24 17:56:08 +0100

    Visualization of histogram and image in GUI

--- app/routes/histogram.py
+++ app/routes/histogram.py
@@ -14,8 +14,23 @@ def histogram():
     """
     form = HistogramForm()
     if form.validate_on_submit():  # POST
-        image_id = form.image.dat
+        image_id = form.image.data
+        my_image = fetch_image(image_id)
 
+        im = iio.imread(config.image_folder_path + '/' + image_id)
+        # calculate mean value from RGB channels and flatten to 1D array
+        vals = im.mean(axis=2).flatten()
+        # calculate histogram
+        counts, bins = np.histogram(vals, range(257))
+        # plot histogram with 255 bins
+        plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
+        plt.title("Image Histogram")
+        plt.xlabel("bin value")
+        plt.ylabel("pixel count")
+        plt.xlim([-0.5, 255.5])
+        plt.savefig(config.image_folder_path + '/' + "my_histogram.png")
+
+        my_histogram = "my_histogram.png"
 
         # returns the image histogram output from the specified image
         # return render_template('classification_output.html', image_id=image_id, results=result_dict)
