2f6d17157f061a433d3ffcfb148b4163649888f4
Mehdi <mehdi.dupont@isen.yncrea.fr>
2022-12-09 14:28:57 +0100

    Removed leftover code

--- app/routes/histogram.py
+++ app/routes/histogram.py
@@ -21,7 +21,6 @@ def histogram():
     form = HistogramForm()
     if form.validate_on_submit():  # POST
         image_id = form.image.data
-        my_image = fetch_image(image_id)
 
         im = iio.imread(config.image_folder_path + '/' + image_id)
         # calculate mean value from RGB channels and flatten to 1D array
@@ -36,11 +35,8 @@ def histogram():
         plt.xlim([-0.5, 255.5])
         plt.savefig(config.image_folder_path + '/' + "my_histogram.png")
 
-        my_histogram = "my_histogram.png"
-
         # returns the image histogram output from the specified image
-        # return render_template('classification_output.html', image_id=image_id, results=result_dict)
-        return render_template("histogram_output.html", image_id=image_id, histogram=my_histogram)
+        return render_template("histogram_output.html", image_id=image_id, histogram="my_histogram.png")
 
     # otherwise, it is a get request and should return the
     # image selector
